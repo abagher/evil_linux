e2fea42a487e60d6c3d35359951510265432e348
saurabh21289 <saurabh.singh21289@gmail.com>
2015-12-16 14:03:26 -0500

    Added comments

--- evil_firefox/sql.py
+++ evil_firefox/sql.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#using sqlite version 3
 import sqlite3 as lite
 import sys
 
-
+#connect with the firefox cookie database
 con = lite.connect('/home/saurabh/.mozilla/firefox/pp0qyr8g.default/cookies.sqlite')
 
+#fetch data from the cookie database
 with con:    
     
     cur = con.cursor()
